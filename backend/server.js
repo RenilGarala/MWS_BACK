d83f93062c69dd6d7c681046869b7bcb659554ec
Sujal Patel <sujal7455@gmail.com>
2026-04-08 01:15:57 +0530

    db connection done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import { app } from "./app.js";
 import "dotenv/config";
+import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  
-})
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error starting server:", error);
+    process.exit(1);
+  });
